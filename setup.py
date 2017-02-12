7318008054baf0b006211019212c1a7dbd500f94
Tim Wentzlau <tim@elektron.fo>
2017-02-12 22:34:44 +0000

    Added info to setup

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ except:
 setup(
     name='kervi-component-library',
     version=VERSION,
+    author='Tim Wentzlau',
+    author_email='tim.wentzlau@gmail.com',
+    url='https://github.com/kervi/kervi-components',
     description="""Component library for kervi""",
     packages=[
         "sensors"
