3fb8ba2587dea672de75687744e4cbb630f3c9b6
Tim Wentzlau <tim@wentzlau.dk>
2018-06-17 23:06:39 +0100

    Add extra parameters

--- kervi/devices/sensors/dummy_sensor.py
+++ kervi/devices/sensors/dummy_sensor.py
@@ -1,34 +1,42 @@
 from kervi.hal import SensorDeviceDriver
 
 class DummySensorDeviceDriver(SensorDeviceDriver):
-    def __init__(self):
+    def __init__(self, **kwargs):
         SensorDeviceDriver.__init__(self)
+
         self.value = 0
-        self.delta = 4
+        self.delta = kwargs.get("delta", 4)
+        self._min = kwargs.get("min", 0)
+        self._max = kwargs.get("max", 100)
+        self._interval = kwargs.get("interval", 1)
+        self._unit = kwargs.get("unit", "C")
+        self._type = kwargs.get("type", "temperature")
 
     def read_value(self):
         self.value += self.delta
-        if self.value == 100:
+        if self.value > self._max:
             self.delta *= -1
-        if self.value == 0:
+            self.value = self._max
+        if self.value < self._min:
             self.delta *= -1
+            self.value = self._min
         return self.value
 
     @property
     def type(self):
-        return "temperature"
+        return self._type
 
     @property
     def unit(self):
-        return "C"
+        return self._unit
 
     @property
     def max(self):
-        return 100
+        return self._max
 
     @property
     def min(self):
-        return 0
+        return self._min
 
 
 class DummyMultiDimSensorDeviceDriver(SensorDeviceDriver):
